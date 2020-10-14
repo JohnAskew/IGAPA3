bfa68470888696d78270d203d394601f86c41e43
JohnAskew <bucbowie@gmail.com>
2020-10-14 15:45:17 -0400

    Add files via upload

---
+++ sql/alex_commit_latency.sql
@@ -0,0 +1,10 @@
+SELECT interval_start
+, min(duration_avg) AS min_dur
+, max(duration_avg) AS max_dur
+, avg(duration_avg) AS avg_dur
+, median(duration_avg) AS median_dur
+FROM exa_sql_hourly 
+WHERE  INTERVAL_start > (current_date - 15)
+     AND command_name='COMMIT' 
+   GROUP BY INTERVAL_start
+   ORDER BY INTERVAL_start;
---
+++ sql/alex_duration_stats_exa_sql_hourly.sql
@@ -0,0 +1,17 @@
+SELECT interval_start 
+, trunc(avg(min_dur),2) AS avg_min
+, trunc(avg(max_dur),2) AS avg_max
+, trunc(avg(avg_dur),2) AS avg_dur
+, trunc(avg(median_dur),2) AS avg_median 
+FROM 
+   ( 
+      SELECT interval_start
+      , min(duration_avg) AS min_dur
+      , max(duration_avg) AS max_dur
+      , avg(duration_avg) AS avg_dur
+      , median(duration_avg) AS median_dur
+    FROM exa_sql_hourly 
+      WHERE command_name='SELECT' GROUP BY INTERVAL_start
+     )
+GROUP BY 1 
+ORDER BY 1 desc;
